18abf85f4dd4682855cd654ea8a94acaba2598fb
hisoka1638 <1399952343@qq.com>
2022-03-27 15:17:12 +0800

    init1

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'My App',
+            title: 'webpack-demo',
             template: 'src/assets/index.html'
         }),
     ],
